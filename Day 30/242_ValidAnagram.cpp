11cb2709f182c2ea7f438b7333dca9eedeb32a4a
Amit Kumar Sah <amitkumarsah1311@gmail.com>
2025-02-15 13:18:06 +0530

    Add files via upload

---
+++ Day 30/242_ValidAnagram.cpp
@@ -0,0 +1,26 @@
+// Given two strings s and t, return true if t is an 
+// anagram
+//  of s, and false otherwise.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+            if(s==t) return true;
+    
+            return false;
+            
+        }
+    };
