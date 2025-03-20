c94547fd7a9df84f912eea2253e670792ef5fed0
Amit Kumar Sah <amitkumarsah1311@gmail.com>
2025-03-20 22:47:28 +0530

    Add files via upload

---
+++ Day 63/78. Subsets.java
@@ -0,0 +1,34 @@
+// 78. Subsets
+// Solved
+// Medium
+// Topics
+// Companies
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [[],[0]]
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> newSubset = new ArrayList<>(result.get(i));
+                newSubset.add(num);
+                result.add(newSubset);
+            }
+        }
+        return result;
+    }
+}
