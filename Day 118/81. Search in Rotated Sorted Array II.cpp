3afa2cf33b3bbdd3d32629b7cc9a9716ab9ef53e
Amit Kumar Sah <amitkumarsah1311@gmail.com>
2025-05-15 00:43:40 +0530

    Add files via upload

---
+++ Day 118/81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,3 @@
+// 81. Search in Rotated Sorted Array II
+// There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+// Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
